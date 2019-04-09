47b514474a5e553e4aa7f37d6b8159c8248f6cea
xuyijun <ayijun@gmail.com>
2019-04-09 11:58:23 +0800

    updated homework 1.1.9~1.1.11

---
+++ src/main/java/ep1/p1_1_1_11.java
@@ -0,0 +1,21 @@
+package ep1;
+
+public class p1_1_1_11 {
+    public static void main(String[] args) {
+
+        Boolean[][] data = new Boolean[4][4];
+        data[0] = new Boolean[]{false, true, false, true};
+        data[1] = new Boolean[]{true, false, true, false};
+        data[2] = new Boolean[]{false, true, false, true};
+        data[3] = new Boolean[]{true, false, true, false};
+
+        for (int i = 0; i < data.length; i++) {
+            System.out.print("L: " + i + "  ");
+            for (int j = 0; j < data[i].length; j++) {
+                System.out.print(j + "->" + (data[i][j] ? " [*] " : " [ ] "));
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ src/main/java/ep1/p1_1_1_9.java
@@ -0,0 +1,17 @@
+package ep1;
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class p1_1_1_9 {
+    public static void main(String[] args) {
+
+        System.out.println("Enter a number: ");
+        int N = StdIn.readInt();
+
+        String s = "";
+        for (int n = N; n > 0; n /= 2)
+            s = (n % 2) + s;
+        System.out.println(s);
+
+    }
+}
